e7243eb39bd8b66d067e3e682fcde1a9b2898736
Michael Herd <michael@next45.co>
2020-02-04 18:30:33 +0200

    Add a Contact page (solves exercise 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contacts" do
+    get static_pages_contacts_url
+    assert_response :success
+    assert_select "title", "Contacts | #{@base_title}"
+  end
 end
