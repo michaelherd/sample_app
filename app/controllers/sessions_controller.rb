7844b2def2977a13954546db5a5cae4dc007ca03
Michael Herd <michael.herd@gmail.com>
2020-02-19 16:46:07 +0200

    Applied Review Comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: create_params[:email].downcase)
+    if user && user.authenticate(create_params[:password])
       log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      create_params[:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid login credentials supplied'
-      render 'new'
+      render :new
     end
   end
 
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def create_params
+    params.require(:session).permit(:email, :password, :remember_me)
   end
 end
